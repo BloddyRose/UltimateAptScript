20d9f8ca299a017d0361bdde27fecc30fd0a1ba6
BloddyRose <bloddyrose@localhost.localdomain>
2020-08-20 14:05:06 +0300

    update

--- ultimateapt.py
+++ ultimateapt.py
@@ -5,6 +5,15 @@ from sys import exit
 import sys
 import glob
 
+'''
+   ___  __        __   __     ___              
+  / _ )/ /__  ___/ /__/ /_ __/ _ \___  ___ ___ 
+ / _  / / _ \/ _  / _  / // / , _/ _ \(_-</ -_)
+/____/_/\___/\_,_/\_,_/\_, /_/|_|\___/___/\__/ 
+                      /___/                    
+            
+'''
+
 """
 Copyright 2020 BloddyRose
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
